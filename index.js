82e00c7e34ac2816fd2f420ca85f9b0691a0684e
Chayan Dutta <dipakchatterjee957@gmail.com>
2025-08-22 01:01:48 +0530

    setup user

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const PORT = process.env.PORT
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
+app.use('/bflp/dev/user', userRouter);
+app.get('/', (req, res) => {
+    res.send('User Service');
+});
+
+app.listen(PORT, () => {
+    console.log(`User Server is running on http://localhost:${PORT}`);
+});
