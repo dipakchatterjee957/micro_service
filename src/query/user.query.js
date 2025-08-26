35876d659c24cda0012bffbfc814fb3c03beb983
Chayan Dutta <dipakchatterjee957@gmail.com>
2025-08-26 21:33:13 +0530

    file changes

--- src/query/user.query.js
+++
@@ -1,5 +0,0 @@
-export default {
-    GET_USER_LIST: `SELECT * FROM user_master 
-    WHERE active_flag = 'A' AND branch_master_id = ? `,
-
-}
--- src/router/user.router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import userController from '../controller/user.controller.js';
-const userRouter = express.Router();
-
-userRouter.get(`/getUserList/:branch_master_id`, userController.getUserList);
-
-export default userRouter;
